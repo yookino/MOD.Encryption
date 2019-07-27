37afccfe47c25047c1037c7611eea8b33f91aa67
Itthipon Treetodbadee <yookino@hotmail.com>
2019-07-27 15:57:56 +0700

    first commit

---
+++ MOD.Encryption/CryptographyHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MOD.Encryption
+{
+    public class CryptographyHelper
+    {
+        public string PrivateKey = "";
+        private static bool _optimalAsymmetricEncryptionPadding = false;
+
+        public CryptographyHelper(string privateKey)
+        {
+            PrivateKey = privateKey;
+        }
+
+        /// <summary>
+        /// ถอดรหัส Token
+        /// </summary>
+        /// <param name="encryptedText">Encrypted Token</param>
+        /// <returns></returns>
+        public string Decrypt(string encryptedText)
+        {
+            int keySize = 0;
+            string publicAndPrivateKeyXml = "";
+
+            GetKeyFromEncryptionString(PrivateKey, out keySize, out publicAndPrivateKeyXml);
+
+            var decrypted = Decrypt(Convert.FromBase64String(encryptedText), keySize, publicAndPrivateKeyXml);
+
+            return Encoding.UTF8.GetString(decrypted);
+        }
+
+        private static byte[] Decrypt(byte[] data, int keySize, string publicAndPrivateKeyXml)
+        {
+            if (data == null || data.Length == 0) throw new ArgumentException("Data are empty", "data");
+            if (!IsKeySizeValid(keySize)) throw new ArgumentException("Key size is not valid", "keySize");
+            if (String.IsNullOrEmpty(publicAndPrivateKeyXml)) throw new ArgumentException("Key is null or empty", "publicAndPrivateKeyXml");
+
+            using (var provider = new RSACryptoServiceProvider(keySize))
+            {
+                provider.FromXmlString(publicAndPrivateKeyXml);
+                return provider.Decrypt(data, _optimalAsymmetricEncryptionPadding);
+            }
+        }
+
+
+        private static void GetKeyFromEncryptionString(string rawkey, out int keySize, out string xmlKey)
+        {
+            keySize = 0;
+            xmlKey = "";
+
+            if (rawkey != null && rawkey.Length > 0)
+            {
+                byte[] keyBytes = Convert.FromBase64String(rawkey);
+                var stringKey = Encoding.UTF8.GetString(keyBytes);
+
+                if (stringKey.Contains("!"))
+                {
+                    var splittedValues = stringKey.Split(new char[] { '!' }, 2);
+
+                    try
+                    {
+                        keySize = int.Parse(splittedValues[0]);
+                        xmlKey = splittedValues[1];
+                    }
+                    catch (Exception e) { }
+                }
+            }
+        }
+
+        private static bool IsKeySizeValid(int keySize)
+        {
+            return keySize >= 384 && keySize <= 16384 && keySize % 8 == 0;
+        }
+
+    }
+}
